c6a8a0d6675a20c579a3b574c207a9205d9c3ef3
Ignacio Diez <ig.diez10@gmail.com>
2014-05-23 01:37:43 -0400

    Landing temporary page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ActionController::Base
+  layout 'application'
+  protect_from_forgery
+
+  def index
+    @name = "Ignacio Diez"
+    @social = {
+      'facebook' => 'https://www.facebook.com/ignacio10',
+      'twitter' => 'https://twitter.com/igdiez',
+      'linkedin' => 'https://cl.linkedin.com/pub/ignacio-diez-albornoz/45/589/b1a'
+     }
+  end
+
+end
