18e354c2ee649c8aaecf922462423362a41dd12c
Ignacio Diez <ig.diez10@gmail.com>
2014-05-26 18:35:13 -0400

    Deploy cfg

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,38 +3,24 @@ lock '3.2.1'
 
 set :application, 'profile'
 set :repo_url, 'git@github.com:ig10/profile.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/mnt/profile'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
+
+  desc 'Bundle Install'
+  task :bundle do
+    on roles(:app) do
+      execute "cd #{release_path} && bundle install"
+    end
+  end
+
+  desc 'Assets Precompile'
+  task :precompile do
+    on roles(:app) do
+      execute "cd #{release_path} && rake assets:precompile"
+    end
+  end
 
   desc 'Restart application'
   task :restart do
@@ -43,7 +29,7 @@ namespace :deploy do
     end
   end
 
-  after :publishing, :restart
+  after :publishing, :bundle, :precompile, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
