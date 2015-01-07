afbd6cafc5ffcf558a64761d15ab3fc45e0a9137
Ignacio Diez <ig.diez10@gmail.com>
2015-01-07 02:33:52 -0300

    Added action to request scrapper

--- app/controllers/scrapper_controller.rb
+++ app/controllers/scrapper_controller.rb
@@ -6,4 +6,13 @@ class ScrapperController < ActionController::Base
     @reference = CanadianWhScrapper::PAGE_URL
   end
 
+  def iec_request
+    forced = params['force'] == '1'
+    old_date = CanadianWhScrapper.last.created_at || Time.now
+    enough_time = ((old_date - Time.now) / 1.hour).round < 0
+    CanadianWhScrapper.execute(request.ip) if enough_time || forced
+
+    redirect_to action: :iec
+  end
+
 end
