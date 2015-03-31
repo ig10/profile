102f7528e6e295e5416d35879bf0341e3b3bc532
Ignacio Diez <ig.diez10@gmail.com>
2015-03-30 22:24:36 -0400

    Added Bundle and Asset precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,23 @@ set :application, 'profile'
 set :repo_url, 'git@github.com:ig10/profile.git'
 set :deploy_to, '/mnt/profile'
 set :keep_releases, 5
+set :rake, 'bundle exec rake'
 
 namespace :deploy do
+
+  desc 'Run Bundler'
+  task :bundle do
+    on roles(:app) do
+      execute "cd #{release_path} && bundle install"
+    end
+  end
+
+  desc 'Precompile Assets'
+  task :precompile do
+    on roles(:app) do
+      execute "cd #{release_path} && rake assets:precompile"
+    end
+  end
 
   desc 'Restart application'
   task :restart do
@@ -15,7 +30,7 @@ namespace :deploy do
     end
   end
 
-  after :publishing, :restart
+  after :publishing, :bundle, :precompile, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -27,3 +42,5 @@ namespace :deploy do
   end
 
 end
+
+
